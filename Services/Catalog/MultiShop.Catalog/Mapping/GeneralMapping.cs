b628dd650ef289e2decdcc1866ba6a54bf44cb10
Anil Yasar <anlysr2002@gmail.com>
2024-04-04 00:11:55 +0300

    Mapping class for DTOs and real entities

---
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+
+            // Buraya diğer nesnelerin DTO larının maplemelerini de koy
+        }
+    }
+}
