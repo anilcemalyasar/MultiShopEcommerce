88a71367b506e66195de008a22a6530df644624c
Anil Yasar <anlysr2002@gmail.com>
2024-04-04 20:30:04 +0300

    category service methods for CRUD operations implemented

--- Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
 using MultiShop.Catalog.Entities;
 
 namespace MultiShop.Catalog.Mapping
@@ -11,6 +14,22 @@ namespace MultiShop.Catalog.Mapping
             CreateMap<Category, ResultCategoryDto>().ReverseMap();
             CreateMap<Category, UpdateCategoryDto>().ReverseMap();
             CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIdProductDto>().ReverseMap();
+
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage,  UpdateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage,  CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage,  GetByIdProductImageDto>().ReverseMap();
+
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail,  UpdateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail,  GetByIdProductDetailDto>().ReverseMap();
 
             // Buraya diğer nesnelerin DTO larının maplemelerini de koy
         }
