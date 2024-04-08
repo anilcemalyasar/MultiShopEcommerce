6348128e3dd5947362b8e3797d8c9161e8e8fa00
Anil Yasar <anlysr2002@gmail.com>
2024-04-08 15:19:22 +0300

    OrderDetail Command and Query Handler classes implemented (CQRS Design Pattern)

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server;initial Catalog=MultiShopOrderDb;integrated Security=true");
+        }
+
+        DbSet<Address> Addresses { get; set; }
+        DbSet<OrderDetail> OrderDetails { get; set; }
+        DbSet<Ordering> Orderings { get; set; }
+
+    }
+}
