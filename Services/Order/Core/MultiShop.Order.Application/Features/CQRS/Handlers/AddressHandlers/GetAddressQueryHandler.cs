8059092b613c1a43b84951a2c4242be079df4ae7
Anil Yasar <anlysr2002@gmail.com>
2024-04-08 12:18:07 +0300

    Address Query Handlers inserted

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAddressQueryResult>> Handle()
+        {
+            var addressList = await _repository.GetAllAsync();
+            return addressList.Select(x => new GetAddressQueryResult
+            {
+                AddressId = x.AddressId,
+                City = x.City,
+                District = x.District,
+                Detail = x.Detail,
+                UserId = x.UserId
+            }).ToList();
+        }
+
+    }
+}
